b8dc4397262e00833346cdd5a44e4bb406e76c2c
Layne <Layne@atguigu.com>
2022-07-13 22:43:54 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author chen
+ * @Description
+ * @create 2022 - 07 - 13 22:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
